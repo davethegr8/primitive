b186aceb8f7d3ef12a3717b07c69f8bb58c7ed6b
Dave Poole <davethegr8@gmail.com>
2017-10-17 19:56:15 -0700

    constrain rect to 4 h

--- primitive/rectangle.go
+++ primitive/rectangle.go
@@ -18,7 +18,7 @@ func NewRandomRectangle(worker *Worker) *Rectangle {
 	x1 := rnd.Intn(worker.W)
 	y1 := rnd.Intn(worker.H)
 	x2 := clampInt(x1+rnd.Intn(32)+1, 0, worker.W-1)
-	y2 := clampInt(y1+rnd.Intn(32)+1, 0, worker.H-1)
+	y2 := clampInt(y1+4, 0, worker.H-1)
 	return &Rectangle{worker, x1, y1, x2, y2}
 }
 
@@ -62,9 +62,11 @@ func (r *Rectangle) Mutate() {
 	case 0:
 		r.X1 = clampInt(r.X1+int(rnd.NormFloat64()*16), 0, w-1)
 		r.Y1 = clampInt(r.Y1+int(rnd.NormFloat64()*16), 0, h-1)
+		r.Y2 = clampInt(r.Y1+4, 0, h-1)
 	case 1:
 		r.X2 = clampInt(r.X2+int(rnd.NormFloat64()*16), 0, w-1)
 		r.Y2 = clampInt(r.Y2+int(rnd.NormFloat64()*16), 0, h-1)
+		r.Y1 = clampInt(r.Y2-4, 0, h-1)
 	}
 }
 
