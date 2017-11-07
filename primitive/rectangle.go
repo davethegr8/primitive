559134a00a0a9f18ac42241d10a4b92a4ac9dcb4
Dave Poole <david.poole@bigfishgames.com>
2017-11-06 16:32:23 -0800

    revert rectangle changes

--- primitive/rectangle.go
+++ primitive/rectangle.go
@@ -17,7 +17,7 @@ func NewRandomRectangle(worker *Worker) *Rectangle {
 	rnd := worker.Rnd
 	x1 := rnd.Intn(worker.W)
 	y1 := rnd.Intn(worker.H)
-	x2 := clampInt(x1+rnd.Intn(4)+1, 0, worker.W-1)
+	x2 := clampInt(x1+rnd.Intn(32)+1, 0, worker.W-1)
 	y2 := clampInt(y1+rnd.Intn(32)+1, 0, worker.H-1)
 	return &Rectangle{worker, x1, y1, x2, y2}
 }
@@ -58,16 +58,13 @@ func (r *Rectangle) Mutate() {
 	w := r.Worker.W
 	h := r.Worker.H
 	rnd := r.Worker.Rnd
-	o := rnd.Intn(4)+1
 	switch rnd.Intn(2) {
 	case 0:
 		r.X1 = clampInt(r.X1+int(rnd.NormFloat64()*16), 0, w-1)
 		r.Y1 = clampInt(r.Y1+int(rnd.NormFloat64()*16), 0, h-1)
-		r.X2 = clampInt(r.X1+o, 0, w-1)
 	case 1:
 		r.X2 = clampInt(r.X2+int(rnd.NormFloat64()*16), 0, w-1)
 		r.Y2 = clampInt(r.Y2+int(rnd.NormFloat64()*16), 0, h-1)
-		r.X1 = clampInt(r.X2-o, 0, w-1)
 	}
 }
 
